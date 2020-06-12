4bbb1aea5a440e8b20748ce45c807d928e6624e8
Natalia Fernández <natalia.fpintos@gmail.com>
2020-06-12 17:46:32 +0100

    Add icons for navigation bar

--- MovieRex/App.js
+++ MovieRex/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import { HighlightsPage } from './js/highlights/highlights-page.js';
 import { GenresPage } from './js/genres/genres-page.js';
@@ -10,7 +11,33 @@ const Tab = createBottomTabNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Highlights':
+                iconName = 'ios-tv';
+                break;
+              case 'Search':
+                iconName = 'ios-search';
+                break;
+              case 'Genres':
+                iconName = 'ios-film';
+                break;
+              case 'Watchlist':
+                iconName = 'ios-bookmark';
+                break;
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#2daeeb',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Highlights" component={HighlightsPage} />
         <Tab.Screen name="Genres" component={GenresPage} />
       </Tab.Navigator>
