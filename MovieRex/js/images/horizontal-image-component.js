3b43b25b45aa3976e14b4f864eefba8cd193e8b1
Natalia Fernández <natalia.fpintos@gmail.com>
2020-06-17 17:35:09 +0100

    Add gradient under horizontal image text for readability

--- MovieRex/js/images/horizontal-image-component.js
+++ MovieRex/js/images/horizontal-image-component.js
@@ -6,6 +6,8 @@ import {
   ImageBackground,
 } from 'react-native';
 
+import LinearGradient from 'react-native-linear-gradient';
+
 export const HorizontalImage = props => {
   const { backdropPath, title } = props;
   debugger
@@ -15,7 +17,13 @@ export const HorizontalImage = props => {
         style={styles.horizontalImage}
         source={{ uri: `https://image.tmdb.org/t/p/w500${backdropPath}` }}
       >
-        <Text style={styles.movieTitle}>{title}</Text>
+        <LinearGradient 
+          colors={['#ffffff00', '#55555500', '#000000dd']} 
+          locations={[0.7, 0.8, 1]}
+          style={styles.linearGradient}
+        >
+          <Text style={styles.movieTitle}>{title}</Text>
+        </LinearGradient>
       </ImageBackground>
     </View>
   );
@@ -33,5 +41,11 @@ const styles = StyleSheet.create({
   movieTitle: {
     fontSize: 16,
     color: '#ffffff'
+  },
+  linearGradient: {
+    height: '100%',
+    justifyContent: 'flex-end',
+    paddingVertical: 2,
+    paddingHorizontal: 5
   }
 });
